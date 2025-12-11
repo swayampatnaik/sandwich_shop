718815547ceab2c4fd3aad501381ead50a9b9fb7
swayampatnaik <swayampatnaik25@gmail.com>
2025-12-11 22:38:34 +0000

    Update main.dart to reduce redundancy after creating common_widgets.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:sandwich_shop/views/cart_screen.dart';
+import 'package:sandwich_shop/views/profile_screen.dart';
+import 'package:sandwich_shop/views/settings_screen.dart';
+import 'package:sandwich_shop/views/about_screen.dart';
+import 'package:sandwich_shop/views/checkout_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,10 +24,18 @@ class App extends StatelessWidget {
       create: (BuildContext context) {
         return Cart();
       },
-      child: const MaterialApp(
+      child: MaterialApp(
         title: 'Sandwich Shop App',
         debugShowCheckedModeBanner: false,
-        home: OrderScreen(maxQuantity: 5),
+        initialRoute: '/order',
+        routes: {
+          '/order': (context) => const OrderScreen(maxQuantity: 5),
+          '/cart': (context) => const CartScreen(),
+          '/checkout': (context) => const CheckoutScreen(),
+          '/profile': (context) => const ProfileScreen(),
+          '/settings': (context) => const SettingsScreen(),
+          '/about': (context) => const AboutScreen(),
+        },
       ),
     );
   }
